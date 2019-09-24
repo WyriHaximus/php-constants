90e8eb24a31d4cb69b462704e0b04aa6e55815ce
Cees-Jan Kiewiet <ceesjank@gmail.com>
2019-09-24 17:21:45 +0200

    Cron constants

---
+++ src/Cron/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace WyriHaximus\Constants\Cron;
+
+const HOURLY = '@hourly';
+const DAILY = '@daily';
+const MIDNIGHT = DAILY;
+const YEARLY = '@yearly';
+const EVERY_MINUTE = '* * * * *';
+const EVERY_5_MINUTES = '*/5 * * * *';
+const EVERY_10_MINUTES = '*/10 * * * *';
+const EVERY_15_MINUTES = '*/15 * * * *';
---
+++ src/Cron/constants_include.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace WyriHaximus\Constants\Cron;
+
+if (!\defined('WyriHaximus\Constants\Cron\HOURLY')) {
+    require __DIR__ . '/constants.php';
+}
