68fc749dc69bf3bc39588336a8a0cef3a7cc56dd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-02-16 00:28:59 +0100

    Add composer autoloader location constant

---
+++ src/ComposerAutoloader/constants.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace WyriHaximus\Constants\ComposerAutoloader;
+
+define('LOCATION', realpath(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'));
--- tests/test.php
+++ tests/test.php
@@ -1,9 +1,14 @@
 <?php
 
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+use Composer\Autoload\ClassLoader;
+
+$autoloader = require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 exit(
     defined('WyriHaximus\Constants\Numeric\ZERO') &&
     defined('WyriHaximus\Constants\HTTPStatusCodes\NOT_FOUND') &&
-    defined('WyriHaximus\Constants\Boolean\TRUE_') ? 0 : 255
+    defined('WyriHaximus\Constants\Boolean\NOT_FOUND') &&
+    defined('WyriHaximus\Constants\ComposerAutoloader\LOCATION') &&
+    $autoloader === require_once LOCATION &&
+    require_once LOCATION instanceof ClassLoader ? 0 : 255
 );
