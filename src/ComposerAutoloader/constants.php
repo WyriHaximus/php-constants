b5d981f5112686d9fc04dd8877670062a4802706
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-02-16 18:56:36 +0100

    Dynamic autoloader locator

--- src/ComposerAutoloader/constants.php
+++ src/ComposerAutoloader/constants.php
@@ -2,4 +2,13 @@
 
 namespace WyriHaximus\Constants\ComposerAutoloader;
 
-define('WyriHaximus\Constants\ComposerAutoloader\LOCATION', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+foreach (array(
+    dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+    dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php',
+) as $location) {
+    if (!file_exists($location)) {
+        continue;
+    }
+
+    define('WyriHaximus\Constants\ComposerAutoloader\LOCATION', $location);
+}
